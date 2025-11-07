e5a3f23db9c7d4918a40e3a6f1d3600435dc72a6
bryan-oginga <ogingabrian2017@gmail.com>
2025-11-07 05:47:00 +0300

    .

--- backend/movieflix/settings.py
+++ backend/movieflix/settings.py
@@ -25,7 +25,7 @@ DEBUG = os.getenv("DEBUG", "True").lower() == "true"
 
 ALLOWED_HOSTS = [
     'api.fitleague.store',
-    'movieflix-cg8e9.ondigitalocean.app',
+    'https://movieflix-5eu4a.ondigitalocean.app',
     '127.0.0.1',
     'localhost',
 ]
@@ -36,7 +36,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://fitleague.store",
-    "movieflix-cg8e9.ondigitalocean.app'",
+    "https://movieflix-5eu4a.ondigitalocean.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
