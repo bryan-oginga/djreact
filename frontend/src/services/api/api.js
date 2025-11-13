5e2a94263dd275342c095d154233ee16942cf00d
bryan-oginga <ogingabrian2017@gmail.com>
2025-11-14 01:06:38 +0300

    tetst

--- frontend/src/services/api/api.js
+++ frontend/src/services/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'https://fitleague.store/api/v1'
+const API_BASE_URL = 'https://api.fitleague.store/api/v1'
 
 const api = axios.create({
   baseURL: API_BASE_URL,
